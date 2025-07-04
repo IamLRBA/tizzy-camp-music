847dc2207bd8fbf9bbfffc312588c74146615cbe
IamLRBA <jerrylarubafestus@gmail.com>
2025-07-05 02:29:33 +0300

    Updated About Page

--- src/components/TeamMemberCard.jsx
+++ src/components/TeamMemberCard.jsx
@@ -5,21 +5,26 @@ import './TeamMemberCard.css';
 const TeamMemberCard = ({ member, isExpanded, isCeo, toggleCard }) => {
   return (
     <div 
-      className={`team-member-card ${isCeo ? 'ceo' : ''} ${isExpanded ? 'expanded' : ''}`}
-      onClick={!isCeo ? toggleCard : undefined}
-      style={{ scrollSnapAlign: isCeo ? 'none' : 'start' }}
+      className={`about-team-member-card ${isCeo ? 'about-ceo' : ''} ${isExpanded ? 'about-expanded' : ''}`}
+      onClick={toggleCard}
     >
-      <div className="team-member-image">
-        <img src={member.image} alt={member.name} />
+      <div className="about-team-member-image-container">
+        <div className="about-team-member-image">
+          <img src={member.image} alt={member.name} />
+        </div>
+        <div className="about-team-member-icon">
+          {member.icon}
+        </div>
       </div>
-      <div className="team-member-info">
+      <div className="about-team-member-info">
         <h3>{member.name}</h3>
-        <p className="team-member-role">{member.role}</p>
+        <p className="about-team-member-role">{member.role}</p>
+        <p className="about-team-member-bio">{member.bio}</p>
         
         {isExpanded && (
-          <div className="team-member-details">
-            <p className="team-member-bio">{member.bio}</p>
-            <div className="team-member-social">
+          <div className="about-team-member-details">
+            <p className="about-team-member-extended-bio">{member.extendedBio}</p>
+            <div className="about-team-member-social">
               {member.social.facebook && (
                 <a href={member.social.facebook} target="_blank" rel="noopener noreferrer">
                   <FaFacebook />
